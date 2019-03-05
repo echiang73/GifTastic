b78906f8d1e9e8d9e508532affe91c8c3a5943a3
Neo <echiang73@gmail.com>
2019-03-05 14:09:09 -0700

    added footer, debugged, add audio but won't play

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,29 @@
 // Global variables: Initial array of topics ----------------------------------------------------------------------
 
 var topics = ["Sea turtle", "Spinner dolphin", "Lanikai Hawaii", "Oahu", "Maui", "Kauai waterfalls", "Hawaii surfing", "Kilauea lava flow"];
+var audioMusic = new Audio("assets/sound/somewhere.mp3");
 
 // Functions ------------------------------------------------------------------------------------------------------
+
+// Function for displaying topic data
+function renderButtons() {
+
+    // Deleting the topics prior to adding new topics (this is necessary otherwise you will have repeat buttons)
+    $("#buttons-view").empty();
+
+    // Looping through the array of topics
+    for (var i = 0; i < topics.length; i++) {
+
+        // Then dynamicaly generating buttons for each topic in the array
+        var a = $("<button>");
+        a.addClass("topic-btn fas fa-umbrella-beach");
+        // a.css("color:blue");
+        // a.addClass("topic-btn fas fa-umbrella-beach style='color:blue;'"); //???????
+        a.attr("data-name", topics[i]);
+        a.text(topics[i]);
+        $("#buttons-view").append(a);
+    }
+}
 
 // Display the appropriate content using displaySearchInfo function to re-render the HTML
 function displaySearchInfo() {
@@ -21,41 +42,38 @@ function displaySearchInfo() {
             var searchDiv = $("<div class='topic'>");
 
             // Storing the rating data
-            var gifRating = response.data[i].rating;
-            var pOne = $("<p>").text("Rating: " + gifRating);
+            var gifRating = (response.data[i].rating);
+            var pOne = $("<p>").text("Rated: " + gifRating.toUpperCase());
+            pOne.css("text-align", "center");
+            pOne.css("background-color", "#f4b87c");
+            pOne.css("width", "200px");
+            pOne.css("border", "2px solid blue")
             searchDiv.append(pOne);
 
             // Retrieving the URL for the image
-            var imgURL = response.data[i].images.original.url;
-            var image = $("<img>").attr("src", imgURL);
-            searchDiv.append(image);
+            // var imgURL = response.data[i].images.original.url;
+            // var image = $("<img>").attr("src", imgURL);
+
+            var image = $("<img>")
+            image.attr("src", response.data[i].images.fixed_width_still.url);
+            image.attr("data-state", "still");
+            image.attr("data-still", response.data[i].images.fixed_width_still.url);
+            image.attr("data-animate", response.data[i].images.fixed_width.url);
+            image.addClass("image");
+            image.css("margin", "-10px 20px 20px 0");
 
             // Putting the entire topic above the previous topics
+            searchDiv.append(image);
             $("#gifs-view").prepend(searchDiv);
+            // $("#gifs-view").prepend(searchDiv, "<p/>"); // why can't I add a hr tag here?
         }
     });
 }
 
-// Function for displaying topic data
-function renderButtons() {
-    // Deleting the topics prior to adding new topics (this is necessary otherwise you will have repeat buttons)
-    $("#buttons-view").empty();
-
-    // Looping through the array of topics
-    for (var i = 0; i < topics.length; i++) {
-
-        // Then dynamicaly generating buttons for each topic in the array
-        var a = $("<button>");
-        a.addClass("topic-btn fas fa-umbrella-beach");
-        // a.css("color:blue");
-        // a.addClass("topic-btn fas fa-umbrella-beach style='color:blue;'"); //???????
-        a.attr("data-name", topics[i]);
-        a.text(topics[i]);
-        $("#buttons-view").append(a);
-    }
-}
-
 // Main processes -------------------------------------------------------------------------------------------------------
+
+// audioMusic.play();
+const playPromise = audioMusic.play();
 
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
@@ -68,6 +86,22 @@ $("#add-search").on("click", function (event) {
     event.preventDefault();
     // This line grabs the input from the textbox
     var topic = $("#search-input").val().trim();
+    if (topic == "") {
+        return false; // so user cannot add a blank button
+    }
     topics.push(topic);
     renderButtons();
+});
+
+// Adding a click event listener to animate/still a gif
+$(document).on("click", ".image", function () {
+    var state = $(this).attr("data-state");
+    if (state == "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    }
+    else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
 });
